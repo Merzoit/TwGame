07fc2e77a963d067c9755769fbfaf93606780903
Your Name <your.email@example.com>
2026-01-02 22:41:09 +0300

    Fix missing PlayerService import in Django views

--- game_app/game/views.py
+++ game_app/game/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_GET, require_POST
+from .services import PlayerService
 import json
 
 # Create your views here.
